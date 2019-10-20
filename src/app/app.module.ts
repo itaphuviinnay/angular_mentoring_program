5205527b53bbf46f02072a4edb5761fb40106770
vinay.itapu <vinay.itapu@thomsonreuters.com>
2019-10-20 20:08:54 +0530

    Task 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddCourseComponent } from './add-course/add-course.component';
+import { CourseListComponent } from './course-list/course-list.component';
+import { HeaderComponent } from './header/header.component';
+import { CoursesComponent } from './courses/courses.component';
+import { CourseComponent } from './course/course.component';
+import { FooterComponent } from './footer/footer.component';
+import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
+import { SearchCourseComponent } from './search-course/search-course.component';
+import { SectionComponent } from './section/section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    AddCourseComponent,
+    CourseListComponent,
+    CourseComponent,
+    HeaderComponent,
+    FooterComponent,
+    BreadcrumbComponent,
+    SearchCourseComponent,
+    SectionComponent
   ],
   imports: [
     BrowserModule,
