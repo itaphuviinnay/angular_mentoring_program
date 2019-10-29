1c1fe26d2c39a737cdc14df4b12a55f35122d293
vinay.itapu <vinay.itapu@thomsonreuters.com>
2019-10-29 10:40:37 +0530

    Task-2 Initial commit

--- src/app/add-course/add-course.component.ts
+++ src/app/add-course/add-course.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-add-course',
   templateUrl: './add-course.component.html',
-  styleUrls: ['./add-course.component.sass']
+  styleUrls: ['./add-course.component.scss']
 })
 export class AddCourseComponent implements OnInit {
 
--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-course',
   templateUrl: './course.component.html',
-  styleUrls: ['./course.component.sass']
+  styleUrls: ['./course.component.scss']
 })
 export class CourseComponent implements OnInit {
 
