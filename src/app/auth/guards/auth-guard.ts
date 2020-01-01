b80e48905798294620ed558d318ed4cc8db4b728
vinay.itapu <vinay.itapu@thomsonreuters.com>
2020-01-01 22:51:45 +0530

    Task-9 partially done

--- src/app/auth/guards/auth-guard.ts
+++ src/app/auth/guards/auth-guard.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { AuthService } from 'src/app/shared/services/auth/auth.service';
+import { Observable, of } from 'rxjs';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
 
-  canActivate(): boolean {
-    const isAuthenticated = this.authService.isAuthenticated$.value;
+  canActivate(): Observable<boolean> {
+    const isAuthenticated = this.authService.isAuthenticated();
     if (isAuthenticated) {
-      return true;
-    } else {
-      this.router.navigateByUrl('login');
-      return false;
+      return of(true);
     }
+    this.router.navigateByUrl('login');
+    return of(false);
   }
 }
--- src/app/search-course/search-course.component.ts
+++ src/app/search-course/search-course.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { debounce } from 'rxjs/operators';
+import { debounce, filter } from 'rxjs/operators';
 import { timer } from 'rxjs';
 
 @Component({
@@ -17,7 +17,13 @@ export class SearchCourseComponent implements OnInit {
   ngOnInit() {
     this.searchControl = new FormControl('');
     this.searchControl.valueChanges
-      .pipe(debounce(() => timer(500)))
+      .pipe(
+        filter(
+          (searchText: string) =>
+            searchText.length === 0 || searchText.length > 2
+        ),
+        debounce(() => timer(500))
+      )
       .subscribe((searchInput: string) =>
         this.courseSearch.emit(searchInput.trim())
       );
